0cb5fce7ebe7f9349f1b64e4e631df165c49b35f
linqingwang <18811442380@163.com>
2019-01-23 20:36:43 +0800

    20190123 deeplearning commit!

--- lesson1.py
+++ lesson1.py
@@ -1,2 +1,10 @@
-print('hello world!')
-#this is the second line!
+import numpy as np
+import pylab
+import matplotlib.pyplot as plt
+import h5py
+from lr_utils import load_dataset
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+# index = 25
+# plt.imshow(test_set_x_orig[index])
+# pylab.show()
+# print("train_set_y=" + str(train_set_y[0][index]))
